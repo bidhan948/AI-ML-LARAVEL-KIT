b7ff8df14f877c7709723582e2564ce9ed442c4c
uddhav.baniya <uddhav.baniya@midastechnologies.com.np>
2024-05-14 16:50:17 +0545

    Maintaining Class

--- src/Services/BidhanAIMLKIT.php
+++ src/Services/BidhanAIMLKIT.php
@@ -4,18 +4,33 @@ namespace Bidhan\AiMlKit\Services;
 
 class BidhanAIMLKIT
 {
-    const PY_SRC = "vendor/bidhan/aimlkit/src/Python/sentiment_analysis.py"; // "packages/Bidhan/AiMlKit/src/Python/sentiment_analysis.py"
+    public $PY_SRC = "vendor/bidhan/aimlkit/src/Python/sentiment_analysis.py";
 
     public function getSentiments($text)
     {
         try {
             $output = null;
             $retval = null;
-            exec(config("aimlkit.PY_EXECUTABLE_PATH") . " " . base_path(BidhanAIMLKIT::PY_SRC) . " " . escapeshellarg($text), $output, $retval);
+            exec(config("aimlkit.PY_EXECUTABLE_PATH") . " " . base_path($this->PY_SRC) . " " . escapeshellarg($text), $output, $retval);
             return json_decode($output[0], true);
         } catch (\Exception $e) {
             info($e->getMessage());
             return $e->getMessage();
         }
+    }
+
+    /**
+     * Dynamically handle calls into the BidhanAIMLKIT instance.
+     *
+     * @param string $method
+     * @param array<mixed> $parameters
+     * @return $this|mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if (method_exists($this, $method)) {
+            return $this->$method(...$parameters);
+        }
+        throw new \UnexpectedValueException("Method [{$method}] does not exist on BidhanAIMLKIT instance.");
     }
 }
